ad1672ce5159ab087dd186233f08996a67a20cbb
Ricardo Santos <Ricardo Santos@Ricardo>
2015-09-27 12:19:46 +0100

    first commit

---
+++ src/ola/Ola.java
@@ -0,0 +1,13 @@
+package ola;
+
+/**
+ *
+ * @author Ricardo Santos
+ */
+public class Ola {
+
+    public static void main(String[] args) {
+        System.err.println("Viva o Benfica");
+    }
+    
+}
